8ed44433a355da0a416adfd6ef74d5096f92c733
simon <bernard.simon.90@gmail.com>
2013-09-14 23:27:37 +0200

    completed - to be tested

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,36 @@
 (function($){
     $.fn.extend({
 
-        slider:function(){
-            return (function(self){
+        slider:function(options){
+            return (function(self, options){
                 var slides = self.find('#slidesWrapper > li'),
                     slidesNb = slides.length,
+                    hovering = false,
 
-                    setupControls = function(){
-                        self.find('#controlsWrapper > .control').on('click',function(e){
+                    showSlide = function(index){
+                        index = (index < 0)?slidesNb -1:index;
+                        index = (index > slidesNb -1)?0:index;
+
+
+                        slides.filter('.active').removeClass('active').fadeOut(500);
+                        $(slides.get(index)).addClass('active').fadeIn(300);
+                        ;
+                    },
+
+                    setup = function(){
+                        self.on({
+
+                            mouseenter: function() {hovering = true;},
+                            mouseleave: function() {hovering = false;}
+
+                        }).find('#controlsWrapper > .control').on('click',function(e){
                             e.preventDefault();
                             theControl = $(this);
-                            index = self.find('#slidesWrapper > .active').index();
-                            index = theControl.hasClass('right')?index+1:index-1;
-                            console.log(index);
+                            index = slides.filter('.active').index();
+
+                            if(e.target.tagName == 'A'){
+                                showSlide(theControl.hasClass('right')?index+1:index-1);
+                            }
                         });
                     },
 
@@ -22,14 +40,24 @@
                         index = (index < 0)?0:index;
                     },
 
+                    timer = function(delay){
+                        setTimeout(function(){
+                            if(!hovering){
+                                showSlide(slides.filter('.active').index() + 1);
+                            };
+                            timer(delay);
+                        },delay);
+                    }
+
                     construct = (function(){
-                        // slidesCount
-                        setupControls();
+                        timer((options.delay !== undefined)?options.delay:5000);
+                        setup();
+                        slides.first().addClass('active').show().siblings().hide();
                     })();
 
                 return {};
 
-            })($(this));
+            })($(this), (options !== undefined)?options:{});
         }
     });
 })(jQuery);
